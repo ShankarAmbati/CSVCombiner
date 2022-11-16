8dd7e330fdcfcb7ecb8df253439173b44d2842ed
ShankarAmbati <76948726+ShankarAmbati@users.noreply.github.com>
2022-11-16 16:28:58 -0600

    Added few test cases

--- pmg/PMG/src/CodingAssesmentTest/PMGAssignmentAppTest.java
+++ pmg/PMG/src/CodingAssesmentTest/PMGAssignmentAppTest.java
@@ -8,6 +8,22 @@ public class PMGAssignmentAppTest {
 
 		testWhenUnknownFileIsPassed();
 		testWhetherDirectoryExists();
+		testFileNameWithSpecialChars();
+	}
+
+	private static void testFileNameWithSpecialChars() {
+		// TODO Auto-generated method stub
+		final PMGAssignmentApp app = new PMGAssignmentApp();
+		String[] args = new String[3];
+		args[0] = "unknownfil??eName?????.csv";
+		args[1] = ">";
+		args[2] = "combinedFile.csv";
+		try {
+			app.main(args);
+		} catch (Throwable th) {
+			assert th.getMessage().equals("UnkownfileName");
+		}
+		
 	}
 
 	public static void testWhenUnknownFileIsPassed() {
